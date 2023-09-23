8838339249bc27d6227dba3678deabad0c47b47f
miwanoff <marina.iva.noffa@gmail.com>
2023-09-23 12:52:01 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// асоціативний масив слів та їх перекладів
+const words = {
+  apple: "яблуко",
+  house: "будинок",
+  mouse: "миша",
+};
+
+let mistakes = 0;
+let correctAnsws = 0;
+loop1:
+while (true) {
+  for (const key in words) {
+    const element = words[key];
+    //alert(key);
+    let input = prompt("Input translation of " + key);
+    if (input == "" || input == "!") {
+      alert("!!!");
+      break loop1;
+    }
+    if (input === words[key]) {
+      alert("!");
+      correctAnsws++;
+    } else {
+      alert("?");
+      mistakes++;
+    }
+    alert(key + " " + words[key]);
+  }
+}
+alert(correctAnsws + " " + mistakes);
